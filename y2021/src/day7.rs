443b4fc2689e3771b291fd7839261b5fb3630ec9
equalis3r <thaison.tran.lsc@gmail.com>
2021-12-07 15:52:46 +0100

    Day 7 solution

--- y2021/src/day7.rs
+++ y2021/src/day7.rs
@@ -1,35 +1,65 @@
-const INPUT: &str = include_str!("./assets/day3.txt");
+const INPUT: &str = include_str!("./assets/day7.txt");
 
 pub fn solve() -> String {
-    let input = parse(INPUT);
-    format!("  Part 1: {}\n  Part 2: {}", part1(&input), part2(&input))
+    let mut input = parse(INPUT);
+    format!(
+        "  Part 1: {}\n  Part 2: {}",
+        part1(&mut input),
+        part2(&input)
+    )
 }
 
-pub fn part1(input: &[usize]) -> usize {
-    todo!()
+pub fn part1(input: &mut [i32]) -> i32 {
+    let length = input.len();
+    input.sort();
+    let median = {
+        if length % 2 == 1 {
+            input[length / 2]
+        } else {
+            (input[length / 2 - 1] + input[length / 2]) / 2
+        }
+    };
+    input.iter().map(|x| (x - median).abs()).sum()
 }
-pub fn part2(input: &[usize]) -> usize {
-    todo!()
+pub fn part2(input: &[i32]) -> i32 {
+    let avg = input.iter().sum::<i32>() / input.len() as i32;
+    input
+        .iter()
+        .fold([0, 0], |acc, &x| {
+            [acc[0] + cost_f(x, avg), acc[1] + cost_f(x, avg + 1)]
+        })
+        .into_iter()
+        .min()
+        .unwrap()
 }
 
-fn parse(input: &str) -> Vec<usize> {
-    todo!()
+fn cost_f(x: i32, x0: i32) -> i32 {
+    (x - x0).abs() * ((x - x0).abs() + 1) / 2
+}
+
+fn parse(input: &str) -> Vec<i32> {
+    input
+        .trim()
+        .split(',')
+        .map(str::parse::<i32>)
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const TEST_INPUT: &str = "";
+    const TEST_INPUT: &str = "16,1,2,0,4,2,7,1,2,14";
 
     #[test]
     fn test_part1() {
-        let parse_test_input = parse(TEST_INPUT);
-        assert_eq!(part1(&parse_test_input), 7);
+        let mut test_input = parse(TEST_INPUT);
+        assert_eq!(part1(&mut test_input), 37);
     }
 
     #[test]
     fn test_part2() {
-        let parse_test_input = parse(TEST_INPUT);
-        assert_eq!(part2(&parse_test_input), 5);
+        let test_input = parse(TEST_INPUT);
+        assert_eq!(part2(&test_input), 168);
     }
 }
